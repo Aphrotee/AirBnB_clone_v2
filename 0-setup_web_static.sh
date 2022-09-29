441219401e247b112aac5ffb8fa84026d31db3f9
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-09-29 08:29:50 -0700

    task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Sets up my web servers for the deployment of web_static.
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+sudo service nginx start
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo 'Hello World' | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
+sudo sed -i '/http {$/a \\tserver {\n\t\tlocation /hbnb_static {\n\t\t\talias /data/web_static/current/;\n\t\t\tdisable_symlinks off;\n\t\t}\n\t}' /etc/nginx/nginx.conf
+sudo sed -i '/server_name _;$/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tdisable_symlinks off;\n\t}' /etc/nginx/sites-enabled/default
+sudo nginx -t
+sudo nginx -s reload
