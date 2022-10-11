023192d6e83e8400df0f066461dc7df5755110a3
Aiyeboan Temitope <aphrotemitope37@gmail.com>
2022-10-11 11:52:17 -0700

    automate fabric installation

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['18.209.20.255', '34.73.76.135']
+env.hosts = ['3.238.176.253', '44.197.205.40']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
