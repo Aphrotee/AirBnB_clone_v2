4e097603ff5087abde7cd37bab0a6b9fb882ac30
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-10-14 04:17:46 +0000

    tasks 0, 1, 2, 3

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,11 +9,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_hbnb():
-    return "Hello HBNB!"
+    """Returns string when queried"""
+    return 'Hello HBNB!'
 
 @app.route('/hbnb')
 def hbnb():
-    return "HBNB"
+    """Returns string when queried"""
+    return 'HBNB'
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0.:5000")
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This a script that starts a Flask web application
+"""
+
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_hbnb():
+    """Returns string when queried"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hbnb():
+    """Returns string when queried"""
+    return 'HBNB'
+
+@app.route('/c/<text>')
+def c_route(text):
+    """Returns string when queried"""
+    text = text.replace('_', ' ')
+    return 'C %s' % text
+
+@app.route('/python', defaults={'text': 'is_cool'})
+@app.route('/python/', defaults={'text': 'is_cool'})
+@app.route('/python/<text>')
+def python_route(text='is_cool'):
+    """Returns string when queried"""
+    text = text.replace('_', ' ')
+    return 'Python %s' % escape(text)
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
