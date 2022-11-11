0c062b72c4455df0603803d0d699fcbae40a9476
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2022-11-11 01:33:54 +0000

    task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,7 +8,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/airbnb-onepage/')
 def hello_hbnb():
     """Returns string when queried"""
     return 'Hello HBNB!'
